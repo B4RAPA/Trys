9113d12fb44c326a7bf50f82e71462a7cc54e6e0
B4RAPA <164010522+B4RAPA@users.noreply.github.com>
2024-08-06 10:26:51 +0200

    Update push_swap.h

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -1,18 +1,32 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-typedef struct t_node
+# include <stdio.h>
+# include <stdlib.h>
+
+typedef struct s_list
 {
-	int value;
-	struct t_node* next;
-	struct t_node* prev;
-}				t_node;
+	int				value;
+	struct s_list* 	next;
+	struct s_list* 	prev;
+}				t_list;
 
 
-void	sort_three_numbers(t_node **stack);
-int 	is_sorted(t_node* stack);
-void 	push_one(t_node *stack_a, t_node *stack_b);
-void 	sort_five(t_node *stack_a, int stack_size);
-void 	push_two(t_node *stack_a, t_node *stack_b);
-int 	check_stack(t_node *stack_a);
+void	sort_three_numbers(t_list **stack);
+int 	is_sorted(t_list* stack);
+void 	push_one(t_list *stack_a, t_list *stack_b);
+void 	sort_five(t_list *stack_a, int stack_size);
+void 	push_two(t_list *stack_a, t_list *stack_b);
+int 	check_stack(t_list *stack_a);
+
+int		ft_lstsize(t_list *lst);
+void	ft_putendl_fd(char *s, int fd);
+int		ft_atoi(const char *str);
+t_list	*ft_lstnew(void *value);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+char	init_array(int argc, char **argv);
+void	init_stack(int argc, char **argv, t_list *stack_a);
+void	sort_numbers(t_list *stack_a,t_list *stack_b, int size, char *arr);
+int		get_median(char *arr);
+
 #endif
